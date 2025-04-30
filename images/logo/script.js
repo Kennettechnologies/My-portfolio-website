d86fbd898f70af43e97e8a998f6fadb875fbbb07
KEVIN ONYANGO <kevinonyangowanga@gmail.com>
2025-04-30 12:48:07 +0300

    Initial commit: Add portfolio website files

---
+++ images/logo/script.js
@@ -0,0 +1,32 @@
+// Tab functionality
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Form submission functionality
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyiQ11Q3AO3E-m-fL3YLlbgu74AeVjxGxSSV6vLaqdAb0Smc_eTJLNALu3vODQQjCU1rA/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully!";
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
+});
